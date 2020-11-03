eeb9f6a08a8d35e87c9451d6efbecc80e759bfa9
Marcelo <mvilla@humboldt.org.co>
2020-11-03 12:09:41 -0500

    changed paths

--- tests/execution_times.sh
+++ tests/execution_times.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 function timeit () {
-  { time "$@" 2>> err.log ; } 2>&1 | grep real | awk '{print $2}'
+  { time "$@" 2>> logs/err.log ; } 2>&1 | grep real | awk '{print $2}'
 }
 
-for filename in data/test/geofences/*.shp; do
+for filename in data/shp/geofences/*.shp; do
   name=$(echo "$filename" | rev | cut -d "/" -f 1 | rev | cut -d "." -f 1)
-  args=("$filename" data/test/IHEH -c epsg:4326)
-  exec_time=$(timeit python src/create_hf_indicators.py "results/test/hf_indicators_${name}.shp" "${args[@]}")
-  echo "Indicators ${name}:" "$exec_time" >> time.log
-  exec_time=$(timeit python src/create_hf_persistence.py "results/test/hf_persistence_${name}.shp" "${args[@]}")
-  echo "Persistence ${name}:" "$exec_time" >> time.log
+  args=("$filename" data/tif/IHEH -c epsg:4326)
+  exec_time=$(timeit python src/create_hf_indicators.py "results/hf_indicators_${name}.shp" "${args[@]}")
+  echo "Indicators ${name}:" "$exec_time" >> logs/time.log
+  exec_time=$(timeit python src/create_hf_persistence.py "results/hf_persistence_${name}.shp" "${args[@]}")
+  echo "Persistence ${name}:" "$exec_time" >> logs/time.log
 done
